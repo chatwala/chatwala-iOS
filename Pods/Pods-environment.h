dd140dab524b9cc17b03bb980eae88609956d54e
K <mustkre8@gmail.com>
2013-11-07 12:23:07 -0500

    adding Pods to repo
    Signed-off-by: K <mustkre8@gmail.com>

---
+++ Pods/Pods-environment.h
@@ -0,0 +1,38 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Mantle
+#define COCOAPODS_POD_AVAILABLE_Mantle
+#define COCOAPODS_VERSION_MAJOR_Mantle 1
+#define COCOAPODS_VERSION_MINOR_Mantle 3
+#define COCOAPODS_VERSION_PATCH_Mantle 1
+
+// Mantle/extobjc
+#define COCOAPODS_POD_AVAILABLE_Mantle_extobjc
+#define COCOAPODS_VERSION_MAJOR_Mantle_extobjc 1
+#define COCOAPODS_VERSION_MINOR_Mantle_extobjc 3
+#define COCOAPODS_VERSION_PATCH_Mantle_extobjc 1
+
+// SSZipArchive
+#define COCOAPODS_POD_AVAILABLE_SSZipArchive
+#define COCOAPODS_VERSION_MAJOR_SSZipArchive 0
+#define COCOAPODS_VERSION_MINOR_SSZipArchive 3
+#define COCOAPODS_VERSION_PATCH_SSZipArchive 1
+
+// SVProgressHUD
+#define COCOAPODS_POD_AVAILABLE_SVProgressHUD
+#define COCOAPODS_VERSION_MAJOR_SVProgressHUD 1
+#define COCOAPODS_VERSION_MINOR_SVProgressHUD 0
+#define COCOAPODS_VERSION_PATCH_SVProgressHUD 0
+
+// TestFlightSDK
+#define COCOAPODS_POD_AVAILABLE_TestFlightSDK
+#define COCOAPODS_VERSION_MAJOR_TestFlightSDK 2
+#define COCOAPODS_VERSION_MINOR_TestFlightSDK 1
+#define COCOAPODS_VERSION_PATCH_TestFlightSDK 1
+
