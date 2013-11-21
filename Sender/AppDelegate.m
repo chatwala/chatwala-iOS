feb36de18f203d69a4c81fe5d33cf28c8c474361
K <mustkre8@gmail.com>
2013-11-21 17:05:48 -0500

    correctly popping to root when multitasking away from the app
    Signed-off-by: K <mustkre8@gmail.com>

--- Sender/AppDelegate.m
+++ Sender/AppDelegate.m
@@ -87,8 +87,8 @@
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     
-//    UINavigationController * rootVC = (UINavigationController*)self.window.rootViewController;
-//    [rootVC popToRootViewControllerAnimated:NO];
+    UINavigationController * rootVC = (UINavigationController*)self.window.rootViewController;
+    [rootVC popToRootViewControllerAnimated:NO];
 //    id currentVC = rootVC.topViewController;
     
 //    if ([currentVC isKindOfClass:[SenderViewController class]]) {
