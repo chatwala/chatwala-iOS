82d4fecdabc9bfb6903a1578cde7924cb58151df
randall chatwala <randallchatwala@RANDALLs-MacBook-Pro-3.local>
2014-01-07 17:00:17 -0500

    added more tests for ground control kill screen

--- SenderTests/CWGroundControlTests.m
+++ SenderTests/CWGroundControlTests.m
@@ -9,9 +9,11 @@
 #import <XCTest/XCTest.h>
 #import "CWGroundControlManager.h"
 #import <OCMock/OCMock.h>
+#import "AppDelegate.h"
 
 @interface CWGroundControlTests : XCTestCase
 
+@property (nonatomic) id mockSut;
 @property (nonatomic) id mockUserDefaults;
 @property (nonatomic) CWGroundControlManager * sut;
 @end
@@ -25,11 +27,12 @@
     self.mockUserDefaults = [OCMockObject partialMockForObject:[NSUserDefaults standardUserDefaults]];
     
     self.sut = [[CWGroundControlManager alloc] init];
-    
+    self.mockSut = [OCMockObject partialMockForObject:self.sut];
 }
 
 - (void)tearDown
 {
+    [self.mockSut stopMocking];
     [self.mockUserDefaults stopMocking];
     // Put teardown code here; it will be run once, after the last test case.
     [super tearDown];
@@ -47,4 +50,102 @@
     [self.mockUserDefaults verify];
 }
 
+- (void) testRefreshSuccesShouldShowKillScreen
+{
+    //given
+    NSDictionary * someDictionary = OCMOCK_ANY;
+    [[[self.mockSut stub] andReturnValue:@YES] shouldShowKillScreen];
+    [[self.mockSut expect] showKillScreen];
+    
+    //when
+    self.sut.refreshSuccessBlock(someDictionary);
+    
+    //should
+    [self.mockSut verify];
+}
+
+- (void) testRefreshSuccesShouldNotShowKillScreen
+{
+    //given
+    NSDictionary * someDictionary = OCMOCK_ANY;
+    [[[self.mockSut stub] andReturnValue:@NO] shouldShowKillScreen];
+    [[self.mockSut reject] showKillScreen];
+    
+    //when
+    self.sut.refreshSuccessBlock(someDictionary);
+    
+    //should
+    [self.mockSut verify];
+}
+
+- (void) testRefreshFailureShouldShowKillScreen
+{
+    //given
+    [[[self.mockSut stub] andReturnValue:@YES] shouldShowKillScreen];
+    [[self.mockSut expect] showKillScreen];
+    
+    //when
+    self.sut.refreshFailureBlock(OCMOCK_ANY);
+    
+    //should
+    [self.mockSut verify];
+}
+
+- (void) testRefreshFailureShouldNotShowKillScreen
+{
+    //given
+    [[[self.mockSut stub] andReturnValue:@NO] shouldShowKillScreen];
+    [[self.mockSut reject] showKillScreen];
+    
+    //when
+    self.sut.refreshFailureBlock(OCMOCK_ANY);
+    
+    //should
+    [self.mockSut verify];
+}
+
+- (void) testShowKillScreen
+{
+    //given
+    AppDelegate * appDel = [[UIApplication sharedApplication] delegate];
+    id mockNavController = [OCMockObject partialMockForObject:appDel.navController];
+    [[mockNavController expect] pushViewController:OCMOCK_ANY animated:NO];
+    id mockDrawController = [OCMockObject partialMockForObject:appDel.drawController];
+    [[mockDrawController expect] closeDrawerAnimated:NO completion:OCMOCK_ANY];
+    
+    //when
+    [self.sut showKillScreen];
+    
+//    should
+    [mockNavController verify];
+    [mockDrawController verify];
+    
+    //cleanup
+    [mockNavController stopMocking];
+    [mockDrawController stopMocking];
+}
+
+- (void) testShouldShowKillScreenShouldReturnYes
+{
+    //given
+    [[[self.mockUserDefaults stub] andReturnValue:@YES] boolForKey:@"APP_DISABLED"];
+    
+    //when
+    BOOL actual = [self.sut shouldShowKillScreen];
+    
+    //should
+    XCTAssertTrue(actual, @"expecting shouldShowKillScreen to return YES");
+}
+
+- (void) testShouldShowKillScreenShouldReturnNO
+{
+    //given
+    [[[self.mockUserDefaults stub] andReturnValue:@NO] boolForKey:@"APP_DISABLED"];
+    
+    //when
+    BOOL actual = [self.sut shouldShowKillScreen];
+    
+    //should
+    XCTAssertFalse(actual, @"expecting shouldShowKillScreen to return YES");
+}
 @end
