3d6f7deccaa0360ed8ac6c7b40a286d1b814058c
randall chatwala <randallchatwala@RANDALLs-MacBook-Pro-3.local>
2014-01-08 17:00:59 -0500

    removed unused commented code

--- Sender/CoreData/CWDataManager.m
+++ Sender/CoreData/CWDataManager.m
@@ -33,27 +33,6 @@
 - (void) setup
 {
 
-//    [AOCoreDataStackUtilities createCoreDataStackWithModelName:@"ChatwalaModel" andConcurrencyType:NSMainQueueConcurrencyType andCompletionHandler:^(NSManagedObjectContext *moc, NSError *error) {
-//        
-//        self.moc = moc;
-//        
-//        if(error)
-//        {
-//            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-//            
-//            //lets move a the save file to see if we can fix this by erasing the save file
-////            NSURL * newURL = [[storeURL URLByDeletingLastPathComponent] URLByAppendingPathComponent:[NSString stringWithFormat:@"%@_%i", [storeURL lastPathComponent], arc4random_uniform(NSUIntegerMax)]];
-////            NSLog(@"copy core data store to %@", newURL);
-////            NSError * error = nil;
-////            [[NSFileManager defaultManager] moveItemAtURL:storeURL toURL:newURL error:&error];
-////            NSAssert(!error, @"error: %@ backing up save file: %@", error, storeURL);
-//            
-//            abort();
-//            
-//        }
-//    }];
-
-    //note we probably want to pass in a couple more parameters to the persistent store coordinator
     [AOCoreDataStackUtilities createCoreDataStackWithModelName:@"ChatwalaModel" andConcurrencyType:NSMainQueueConcurrencyType options:@{NSMigratePersistentStoresAutomaticallyOption:@YES, NSInferMappingModelAutomaticallyOption:@YES} andCompletionHandler:^(NSManagedObjectContext *moc, NSError *error, NSURL *storeURL) {
      
         self.moc = moc;
