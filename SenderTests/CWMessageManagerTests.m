3a2ba928651dbf3deeaa1bdb405e89bf35579c0b
randall chatwala <randallchatwala@RANDALLs-MacBook-Pro-3.local>
2014-01-09 14:33:03 -0500

    removed warning

--- SenderTests/CWMessageManagerTests.m
+++ SenderTests/CWMessageManagerTests.m
@@ -135,7 +135,8 @@
     NSProgress * progress = OCMOCK_ANY;
     id mockManager = [OCMockObject mockForClass:[AFURLSessionManager class]];
     [[[mockManager stub] andReturn:mockManager] alloc];
-    [[[mockManager stub] andReturn:mockManager] initWithSessionConfiguration:OCMOCK_ANY];
+    id toMakeWarningGoAway = [[[mockManager stub] andReturn:mockManager] initWithSessionConfiguration:OCMOCK_ANY];
+    NSLog(@"%@",toMakeWarningGoAway);
     [[mockManager expect] downloadTaskWithRequest:OCMOCK_ANY progress:[OCMArg setTo:progress] destination:OCMOCK_ANY completionHandler:OCMOCK_ANY];
     
     //when
@@ -146,5 +147,7 @@
     [mockManager stopMocking];
     
 }
+
+
 
 @end
