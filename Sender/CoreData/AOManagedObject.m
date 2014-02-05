8f3f97e0cfa7dd5b5d7a843cb396aa3b7b6edfb2
randall chatwala <randallchatwala@RANDALLs-MacBook-Pro-3.local>
2014-02-05 15:44:34 -0500

    added comment to help future self output in milliseconds

--- Sender/CoreData/AOManagedObject.m
+++ Sender/CoreData/AOManagedObject.m
@@ -119,6 +119,19 @@
         if ((attributeType == NSDateAttributeType) && (dateFormatter != nil)) {
             value = [dateFormatter stringFromDate:value];
         }
+//        if (attributeType == NSDateAttributeType)
+//        {
+//            if(dateFormatter != nil)
+//            {
+//                value = [dateFormatter stringFromDate:value];
+//            }
+//            else
+//            {
+//                NSTimeInterval timeSinceEpoch = [value timeIntervalSince1970];
+//                value = [NSString stringWithFormat:@"%lli",(long long)(timeSinceEpoch * 1000)];
+//                //                value = [NSNumber numberWithLongLong:(timeSinceEpoch * 1000)];//if the value needs to be a number instead of a string
+//            }
+//        }
         
         [jsonDict setValue:value forKey:attribute];
     }
