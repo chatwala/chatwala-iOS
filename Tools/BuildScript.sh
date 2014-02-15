ff2d45d5c041b2c4f75c08e7b77115efdd2b6ada
rahulkumarny <rahul.kumar05@gmail.com>
2014-02-14 23:03:13 -0500

    Updating build script for prod builds.

--- Tools/BuildScript.sh
+++ Tools/BuildScript.sh
@@ -118,16 +118,11 @@ build() {
 }
 
 # build (Project-Scheme, BuildType, CodeSigningIdentity, Profile name, other build flags)
-#build 'Sender' 'AppStore' 'Release' "$cwAppStoreIdentity" 'B7AD3FC8-E51A-4236-9465-BFA74A6E6C7F.mobileprovision' ''
+build 'Sender' 'AppStore' 'Release' "$cwAppStoreIdentity" 'B7AD3FC8-E51A-4236-9465-BFA74A6E6C7F.mobileprovision' 'CW_URL_SCHEME=chatwala'
 
 #development signed builds
-<<<<<<< HEAD
-build 'Sender' 'prod' 'Release' "$cwDebugIdentity" '68D670EB-0A45-433B-9EF5-CA94D0B7197A.mobileprovision' 'USE_STAGING_SERVER=1'
-#build 'Sender' 'dev' 'Release' "$cwDebugIdentity" 'D0D8C84D-6B9B-4F38-B796-D0C7BD3A73E5.mobileprovision' 'USE_DEV_SERVER=1'
-#build 'Sender' 'qa' 'Release' "$cwDebugIdentity" '7A16570A-50AB-4FD7-8BA1-D259DF7654FE.mobileprovision' 'USE_QA_SERVER=1'
-=======
->>>>>>> 082caecd66678518fa25d55967facfc311891a90
 
-#build 'Sender' 'prod' 'Release' "$cwDebugIdentity" '3602CF16-C1C5-4A38-ADA0-1D23786253BD.mobileprovision' ''
+
+build 'Sender' 'prod' 'Release' "$cwDebugIdentity" '68D670EB-0A45-433B-9EF5-CA94D0B7197A.mobileprovision' 'CW_URL_SCHEME=chatwala'
 #build 'Sender' 'dev' 'Release' "$cwDebugIdentity" '2516BD10-731C-4AE3-B9D5-651227406C4E.mobileprovision' 'USE_DEV_SERVER=1  CW_URL_SCHEME=chatwala-dev'
-build 'Sender' 'qa' 'Release' "$cwDebugIdentity" '7A16570A-50AB-4FD7-8BA1-D259DF7654FE.mobileprovision' 'USE_QA_SERVER=1 CW_URL_SCHEME=chatwala-qa'
+#build 'Sender' 'qa' 'Release' "$cwDebugIdentity" '7A16570A-50AB-4FD7-8BA1-D259DF7654FE.mobileprovision' 'USE_QA_SERVER=1 CW_URL_SCHEME=chatwala-qa'
