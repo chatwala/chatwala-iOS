64788e9358be22a22adf2b201b72c2ab3f307a3c
rahulkumarny <rahul.kumar05@gmail.com>
2014-02-19 11:25:31 -0500

    Updating build script version

--- Tools/BuildScript.sh
+++ Tools/BuildScript.sh
@@ -14,7 +14,7 @@ echo $buildNotes
 buildsFolder="../Builds"
 
 appVersion="1.4.0"
-internalVersion="2.7.3"
+internalVersion="2.7.4"
 displayName="chatwala"
 cwDebugIdentity="\"iPhone Developer: Rahul Kumar (59L7REF9QB)\""
 cwAppStoreIdentity="\"iPhone Distribution: Chatwala Inc\""
