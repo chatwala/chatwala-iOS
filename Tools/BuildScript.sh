5820b3b643cd80d675b13491eb61e15f2ad52df2
Kevin Miller <Airswoop1@gmail.com>
2014-02-19 14:58:43 -0500

    incremented build number on build script

--- Tools/BuildScript.sh
+++ Tools/BuildScript.sh
@@ -124,5 +124,5 @@ build() {
 
 
 #build 'Sender' 'prod' 'Release' "$cwDebugIdentity" '68D670EB-0A45-433B-9EF5-CA94D0B7197A.mobileprovision' 'CW_URL_SCHEME=chatwala'
-build 'Sender' 'dev' 'Release' "$cwDebugIdentity" '2516BD10-731C-4AE3-B9D5-651227406C4E.mobileprovision' 'USE_DEV_SERVER=1  CW_URL_SCHEME=chatwala-dev'
-#build 'Sender' 'qa' 'Release' "$cwDebugIdentity" '7A16570A-50AB-4FD7-8BA1-D259DF7654FE.mobileprovision' 'USE_QA_SERVER=1 CW_URL_SCHEME=chatwala-qa'
+#build 'Sender' 'dev' 'Release' "$cwDebugIdentity" '2516BD10-731C-4AE3-B9D5-651227406C4E.mobileprovision' 'USE_DEV_SERVER=1  CW_URL_SCHEME=chatwala-dev'
+build 'Sender' 'qa' 'Release' "$cwDebugIdentity" '7A16570A-50AB-4FD7-8BA1-D259DF7654FE.mobileprovision' 'USE_QA_SERVER=1 CW_URL_SCHEME=chatwala-qa'
