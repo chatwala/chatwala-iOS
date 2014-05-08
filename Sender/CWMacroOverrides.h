3e3003576d9b02a8b728b263f2a871a6bd0ed58c
rahulkumarny <rahul.kumar05@gmail.com>
2014-05-08 11:50:27 -0400

    Setting inbox max size at 300 messages.

--- Sender/CWMacroOverrides.h
+++ Sender/CWMacroOverrides.h
@@ -6,4 +6,5 @@
 
 
 
-//#define     OVERRIDE_USER_ID   @"482e0d1f-5e7e-4ddf-b7ee-25355ec6d91b"
+//#define     OVERRIDE_USER_ID            @"482e0d1f-5e7e-4ddf-b7ee-25355ec6d91b"
+#define     OVERRIDE_INBOX_MAX_SIZE     300
