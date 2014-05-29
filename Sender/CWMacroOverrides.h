26b3121e27148cba4d2a4181506026c3b6fbdf3d
rahulkumarny <rahul.kumar05@gmail.com>
2014-05-29 12:49:52 -0400

    Inbox capped at 300 messages.

--- Sender/CWMacroOverrides.h
+++ Sender/CWMacroOverrides.h
@@ -3,3 +3,8 @@
 //  Sender
 //
 //  Created by Rahul Kumar Sharma on 1/25/14.
+
+
+
+//#define     OVERRIDE_USER_ID            @"482e0d1f-5e7e-4ddf-b7ee-25355ec6d91b"
+#define     OVERRIDE_INBOX_MAX_SIZE     300
