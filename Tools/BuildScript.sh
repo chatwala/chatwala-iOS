c0c7a1d1aa5e7bc38d55c89939ea36ee17030e04
rahulkumarny <rahul.kumar05@gmail.com>
2014-05-29 12:53:02 -0400

    Incrementing build version

--- Tools/BuildScript.sh
+++ Tools/BuildScript.sh
@@ -14,7 +14,7 @@ echo $buildNotes
 buildsFolder="../Builds"
 
 appVersion="1.6.4"
-internalVersion="2.7.43"
+internalVersion="2.7.44"
 displayName="chatwala"
 cwDebugIdentity="\"iPhone Developer: Rahul Kumar (59L7REF9QB)\""
 cwAppStoreIdentity="\"iPhone Distribution: Chatwala Inc\""
